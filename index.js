9ea4dfa4cf7f893dd36c37f005e65b4b4e20f34b
jim.touchtone <jim.touchtone@kresa.org>
2024-03-27 16:06:49 -0400

    Try lower case 'ember.libraries.register('Ember Paper...

--- index.js
+++ index.js
@@ -464,7 +464,7 @@ module.exports = {
 
     let versionTree = writeFile(
       'ember-paper/register-version.js',
-      `const Ember = require('ember'); Ember.libraries.register('Ember Paper', '${version}');`
+      `ember.libraries.register('Ember Paper', '${version}');`
     );
 
     let hammerJs = fastbootTransform(new Funnel(this.pathBase('hammerjs'), {
